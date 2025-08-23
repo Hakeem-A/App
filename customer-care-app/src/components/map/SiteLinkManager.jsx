3bdb49b9e1d7b713d2268ad224cd8aa2a323bf5c
Abdi Mohamed <bashanesix@gmail.com>
2025-08-23 13:48:17 +0300

    added map features

--- customer-care-app/src/components/map/SiteLinkManager.jsx
+++ customer-care-app/src/components/map/SiteLinkManager.jsx
@@ -8,31 +8,30 @@ const SiteLinkManager = ({ map, sites, layerGroup }) => {
     // Clear existing connections
     layerGroup.clearLayers();
 
-    // Create an array of site coordinates
-    const siteCoordinates = sites.map(site => [site.lat, site.lng]);
-
-    // Draw a polyline connecting all sites with solid blue lines
-    if (siteCoordinates.length > 1) {
-      const polyline = L.polyline(siteCoordinates, {
-        color: '#007bff', // Solid blue color
-        weight: 5,
-        opacity: 1 // Solid line
-      }).addTo(layerGroup);
-
-      polyline.bindPopup('Connection between all sites');
-      
-      // Add dots at each site location where the line meets a site
-      sites.forEach(site => {
-        const dot = L.circleMarker([site.lat, site.lng], {
-          radius: 9,
-          fillColor: '#007bff',
-          color: '#ffffff',
-          weight: 3,
-          opacity: 1,
-          fillOpacity: 1
-        }).addTo(layerGroup);
-        
-        dot.bindPopup(`Connection point: ${site.name}`);
+    // Create routes between consecutive sites using OSRM
+    if (sites.length > 1) {
+      sites.forEach((site, index) => {
+        if (index < sites.length - 1) {
+          const nextSite = sites[index + 1];
+          L.Routing.control({
+            waypoints: [
+              L.latLng(site.lat, site.lng),
+              L.latLng(nextSite.lat, nextSite.lng)
+            ],
+            router: L.Routing.osrmv1({
+              serviceUrl: 'https://router.project-osrm.org/route/v1'
+            }),
+            lineOptions: {
+              styles: [{color: '#007bff', weight: 5}],
+              extendToWaypoints: false,
+              missingRouteTolerance: 10
+            },
+            show: false,
+            addWaypoints: false,
+            routeWhileDragging: false,
+            createMarker: () => null
+          }).addTo(map);
+        }
       });
     }
   }, [map, sites, layerGroup]);
